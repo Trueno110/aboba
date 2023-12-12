00e360aa605965df0e745ea01ebd503b231570a6
Trueno110 <Trueno110@yandex.ru>
2023-12-12 10:50:50 +0300

    1Zadanie

--- aboba/Program.cs
+++ aboba/Program.cs
@@ -10,7 +10,40 @@ namespace aboba
     {
         static void Main(string[] args)
         {
-            hiuojhuohiu
+            Zadanie_1();
+        }
+
+        private static void Zadanie_1()
+        {
+            int[] array = { 6, 23, 3, 8, 19, 54, 21, 666, 7, 8734, 5, 2, 111, 78, 96 };
+
+            int Chet = 0;
+            int Nechet = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] % 2 == 0)
+                {
+                    Chet++;
+                }
+                else if (array[i] % 2 != 0)
+                {
+                    Nechet++;
+                }
+            }
+
+
+
+            Console.WriteLine("Количество четных элементов: " + Chet);
+            Console.WriteLine("Количество нечетных элементов: " + Nechet);
+
+            Console.ReadLine();
+        }
+        private static void Zadanie_2()
+        {
+
         }
     }
+
 }
+
+
