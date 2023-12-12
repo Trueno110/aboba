a5a10f019a580edf1dd3d27ece9a0132c1109d07
Trueno110 <Trueno110@yandex.ru>
2023-12-12 10:58:59 +0300

    2_Zadanie

--- aboba/Program.cs
+++ aboba/Program.cs
@@ -10,7 +10,7 @@ namespace aboba
     {
         static void Main(string[] args)
         {
-            Zadanie_1();
+            Zadanie_2();
         }
 
         private static void Zadanie_1()
@@ -32,18 +32,44 @@ namespace aboba
             }
 
 
-
             Console.WriteLine("Количество четных элементов: " + Chet);
             Console.WriteLine("Количество нечетных элементов: " + Nechet);
 
             Console.ReadLine();
         }
+       
+        
         private static void Zadanie_2()
         {
+            Console.WriteLine("Введите размер массива:");
+            int size = Convert.ToInt32(Console.ReadLine());
 
+            int[] array = new int[size];
+            Console.WriteLine("Введите размер чисел:");
+
+            for (int i = 0; i < size; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Введите значение для фильтрации:");
+            int filterValue = Convert.ToInt32(Console.ReadLine());
+
+            int count = 0;
+            foreach (int num in array)
+            {
+                if (num < filterValue)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("Количество значений меньше "+ filterValue+ " : "+count);
         }
     }
-
 }
+    
+
+
 
 
